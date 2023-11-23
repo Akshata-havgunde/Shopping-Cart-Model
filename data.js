c07c9713c1a3eda3e9004902228fe17780c6a6c1
Akshata-havgunde <112488039+Akshata-havgunde@users.noreply.github.com>
2023-11-23 17:23:11 +0530

    Shopping cart Model

---
+++ data.js
@@ -0,0 +1,30 @@
+let shopItemsData = [
+  {
+    id: "jfhgbvnscs",
+    name: "Casual Shirt",
+    price: 200,
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+    img: "images/img-1.jpg",
+  },
+  {
+    id: "ioytrhndcv",
+    name: "Dresses",
+    price: 500,
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+    img: "images/img-5.jpg",
+  },
+  {
+    id: "wuefbncxbsn",
+    name: "Tops & Tunics",
+    price: 300,
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+    img: "images/img-8.jpg",
+  },
+  {
+    id: "thyfhcbcv",
+    name: "Ethnic Wear",
+    price: 700,
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+    img: "images/img-9.jpg",
+  },
+];
